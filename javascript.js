1d9ee120a9c9c4cbc3905d6f80d8ba14fafb6a64
svtankhuong <tankhuongpham35@gmail.com>
2024-11-13 15:29:28 +0700

    Lưu lại giao diện giỏ hàng đang làm. Giao diện giỏ hàng làm chưa xong.

--- javascript.js
+++ javascript.js
@@ -12,3 +12,38 @@ function showMoreProducts() {
   button.textContent =
     button.textContent === "Hiển thị thêm" ? "Thu gọn" : "Hiển thị thêm";
 }
+// su kien gio hang
+document.querySelector('.cart').addEventListener('click',() =>{
+  document.querySelector('.container.slider-banner').style.display = 'none';
+  document.querySelectorAll('.container.suggestion').forEach(div => {
+    div.style.display = 'none';
+  });
+  document.querySelector('.main').innerHTML = `
+  <h1 id="gio_hang">Giỏ hàng của bạn</h1>
+  <div id="bang_gh">
+    <div id="phone_area">
+      <div>Điện thoại</div>
+      <hr>
+      <div></div>
+    </div>
+    <div id="price_area">
+      <div>Đơn giá</div>
+      <hr>
+      <div>VNĐ</div>
+    </div> 
+    <div id="quantity_area">
+      <div>Số lượng</div>
+      <hr>
+      <div id="control_quantity">
+        <button>-</button>
+        <div id="quantity">0</div>
+        <button>+</button>
+      </div>
+    </div>
+    <div id="total_area">
+      <div>Tổng tiền</div>
+      <hr>
+      <div>VNĐ</div>
+    </div>
+  </div>`
+});
